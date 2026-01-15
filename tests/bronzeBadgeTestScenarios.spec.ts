32be5dbe5c0dcc0217eecb2f7440259dfc6a2588
Ana May Pereira <ana.pereira@3cloudsolutions.com>
2026-01-15 15:00:53 +0800

    Initial Playwright script - Bronze

---
+++ tests/bronzeBadgeTestScenarios.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeAll(async ({ page }) => {
+  await page.goto('https://3cloudsolutions.com/');
+  await expect(page).toHaveTitle('3Cloud');
+});
+
+test('Who We Serve header dropdown', async ({ page }) => {
+  await page.getByRole('link', {name: 'Who We Serve'}).hover();
+  await expect(page.getByText('INDUSTRIES')).toBeVisible(); 
+});
+
+test('Financial Services link', async ({ page }) => {
+  await page.getByRole('link', {name: 'Financial Services'}).click();
+  
+});
+
+test('Lets talk button new page', async ({page}) => {
+
+});
+
+test('error message for incorrect email', async ({page}) => {
+
+});
+
+test('error message for phone number', async ({page}) => {
+
+});
+
+test('successful submission of Get Started inquiry', async ({page}) => {
+
+});
